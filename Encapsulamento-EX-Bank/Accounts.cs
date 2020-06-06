db50ded95cab8867b47c0c6c553b8cf6b9b93fe0
Bruno Alves <brunoalveslp200597@gmail.com>
2020-06-06 14:20:53 -0300

    new study about encapsulation

---
+++ Encapsulamento-EX-Bank/Accounts.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Encapsulamento_ex_banco
+{
+    class Accounts
+    {
+        public int Account { get; private set; }
+        public string Name { get; set; }
+        public double Balance { get; private set; }
+
+        public Accounts(int account, string name)
+        {
+            Account = account;
+            Name = name;
+            Balance = 0;
+        }
+
+        public Accounts(int account, string name ,double balance) : this(account, name)
+        {
+            Balance = balance;
+        }
+
+
+        public void deposit(double deposit)
+        {
+             Balance += deposit;
+        }
+
+        public void withdraw(double withdraw)
+        {
+            Balance -= withdraw + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {Account}, Titular: {Name}, Saldo: {Balance,8:c}";
+        }
+
+
+    }
+}
---
+++ Encapsulamento-EX-Bank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento_ex_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts conta;
+
+            Console.WriteLine("----------------------------------------------------------");
+            Console.Write("Entre Com o numero da conta: ");
+            int Numero = int.Parse(Console.ReadLine());
+            Console.Write("Entro o Titular da conta: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Havera deposito inicial? (s/n)");
+            char res = char.Parse(Console.ReadLine());
+            if (res =='s' || res == 'S')
+            {
+                Console.Write("Quantia a ser Depositada: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Accounts(Numero, Nome, deposito);
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Dados da Conta: ");
+                Console.WriteLine(conta);
+            } else
+            {
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Dados da Conta: ");
+                conta = new Accounts(Numero, Nome);
+                Console.WriteLine(conta);
+            }
+            Console.WriteLine("--------------------------------------------------------");
+            Console.Write("Entre com um valor para Deposito: ");
+            double deposit = double.Parse(Console.ReadLine());
+            conta.deposit(deposit);
+            Console.WriteLine("Dados Atualizados da Conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine("--------------------------------------------------------");
+            Console.Write("Entre com um valor para Saque: ");
+            double withdraw = double.Parse(Console.ReadLine());
+            conta.withdraw(withdraw);
+            Console.WriteLine("Dados Atualizados da Conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine("-------------------------------------------------------");
+
+
+        }
+    }
+}
